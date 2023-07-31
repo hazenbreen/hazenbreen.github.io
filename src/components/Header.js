826066d30663cb296ab5edf964db1649e0d246c5
Hazen Breen <hazenbreen@gmail.com>
2023-07-31 17:02:39 -0400

    REDESIGN! worked off template from Dorata Gil

---
+++ src/components/Header.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import Typical from "react-typical";
+import Switch from "react-switch";
+
+class Header extends Component {
+  titles = [];
+
+  constructor() {
+    super();
+    this.state = { checked: false };
+    this.onThemeSwitchChange = this.onThemeSwitchChange.bind(this);
+  }
+
+  onThemeSwitchChange(checked) {
+    this.setState({ checked });
+    this.setTheme();
+  }
+
+  setTheme() {
+    var dataThemeAttribute = "data-theme";
+    var body = document.body;
+    var newTheme =
+      body.getAttribute(dataThemeAttribute) === "dark" ? "light" : "dark";
+    body.setAttribute(dataThemeAttribute, newTheme);
+  }
+
+  render() {
+    if (this.props.sharedData) {
+      var name = this.props.sharedData.name;
+      this.titles = this.props.sharedData.titles.map(x => [ x.toUpperCase(), 1500 ] ).flat();
+    }
+
+    const HeaderTitleTypeAnimation = React.memo( () => {
+      return <Typical className="title-styles" steps={this.titles} loop={50} />
+    }, (props, prevProp) => true);
+
+    return (
+      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
+        <div className="row aligner" style={{height: '100%'}}>
+          <div className="col-md-12">
+            <div>
+              <span className="header-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="80" height="80" fill="currentColor" class="bi bi-braces-asterisk" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M1.114 8.063V7.9c1.005-.102 1.497-.615 1.497-1.6V4.503c0-1.094.39-1.538 1.354-1.538h.273V2h-.376C2.25 2 1.49 2.759 1.49 4.352v1.524c0 1.094-.376 1.456-1.49 1.456v1.299c1.114 0 1.49.362 1.49 1.456v1.524c0 1.593.759 2.352 2.372 2.352h.376v-.964h-.273c-.964 0-1.354-.444-1.354-1.538V9.663c0-.984-.492-1.497-1.497-1.6ZM14.886 7.9v.164c-1.005.103-1.497.616-1.497 1.6v1.798c0 1.094-.39 1.538-1.354 1.538h-.273v.964h.376c1.613 0 2.372-.759 2.372-2.352v-1.524c0-1.094.376-1.456 1.49-1.456v-1.3c-1.114 0-1.49-.362-1.49-1.456V4.352C14.51 2.759 13.75 2 12.138 2h-.376v.964h.273c.964 0 1.354.444 1.354 1.538V6.3c0 .984.492 1.497 1.497 1.6ZM7.5 11.5V9.207l-1.621 1.621-.707-.707L6.792 8.5H4.5v-1h2.293L5.172 5.879l.707-.707L7.5 6.792V4.5h1v2.293l1.621-1.621.707.707L9.208 7.5H11.5v1H9.207l1.621 1.621-.707.707L8.5 9.208V11.5h-1Z"/>
+                </svg>
+              </span>
+              <br/>
+              <h1 className="mb-0">
+                <Typical steps={[name]} wrapper="p" />
+              </h1>
+              <div className="title-container">
+                <HeaderTitleTypeAnimation />
+              </div>
+              <Switch
+                checked={this.state.checked}
+                onChange={this.onThemeSwitchChange}
+                offColor="#56748B"
+                onColor="#1E1E1E"
+                className="react-switch mx-auto"
+                width={90}
+                height={40}
+                uncheckedIcon={
+                  <span
+                    className="iconify"
+                    data-icon="twemoji:owl"
+                    data-inline="false"
+                    style={{
+                      display: "block",
+                      height: "100%",
+                      fontSize: 25,
+                      textAlign: "end",
+                      marginLeft: "20px",
+                    }}
+                  ></span>
+                }
+                checkedIcon={
+                  <span
+                    className="iconify"
+                    data-icon="noto-v1:sun-with-face"
+                    data-inline="false"
+                    style={{
+                      display: "block",
+                      height: "100%",
+                      fontSize: 25,
+                      textAlign: "end",
+                      marginLeft: "10px",
+                    }}
+                  ></span>
+                }
+                id="icon-switch"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
